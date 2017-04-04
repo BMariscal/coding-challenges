a10f54749b6f3853af85f644a3d540c153e945bb
Briceida Mariscal <BMariscal@users.noreply.github.com>
2017-04-03 23:04:00 -0700

    update

--- miscellaneous/fizz_buzz.py
+++ miscellaneous/fizz_buzz.py
@@ -3,29 +3,37 @@
 
     #Multiples of 3 -> 'Fizz'
     #Multiples of 5 -> 'Buzz'
-    #Multiples of 3 and 5 -> 'FizzBuzz'
+#     #Multiples of 3 and 5 -> 'FizzBuzz'
 
-expected = [
-    '1',
-    '2',
-    'Fizz',
-    '4',
-    'Buzz',
-    'Fizz',
-    '7',
-    '8',
-    'Fizz',
-    'Buzz',
-    '11',
-    'Fizz',
-    '13',
-    '14',
-    'FizzBuzz'
-]
+
+#Expected:
+# None -> Exception
+# < 1 -> Exception
+# expected = [
+#     '1',
+#     '2',
+#     'Fizz',
+#     '4',
+#     'Buzz',
+#     'Fizz',
+#     '7',
+#     '8',
+#     'Fizz',
+#     'Buzz',
+#     '11',
+#     'Fizz',
+#     '13',
+#     '14',
+#     'FizzBuzz'
+# ]
 
 
 
 def fizz_buzz(num):
+    if num is None:
+        raise TypeError('num cannot be None')
+    if num < 1:
+        raise ValueError('num cannot be less than one')
     arr =[]
     for i in range(1,num+1):
         if i%3 == 0 and i% 5 == 0:
