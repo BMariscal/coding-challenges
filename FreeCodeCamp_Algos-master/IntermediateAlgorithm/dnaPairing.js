ff3285234e93fe9fef09ea6fff5a0e8075519b9d
Briceida Mariscal <BMariscal@users.noreply.github.com>
2017-04-07 00:20:27 -0700

    update

--- FreeCodeCamp_Algos-master/IntermediateAlgorithm/dnaPairing.js
+++ FreeCodeCamp_Algos-master/IntermediateAlgorithm/dnaPairing.js
@@ -45,3 +45,8 @@ function pairElement(str) {
 
 
 pairElement("GCG");
+
+
+ pairElement("ATCGA")// should return [["A","T"],["T","A"],["C","G"],["G","C"],["A","T"]].
+pairElement("TTGAG") //should return [["T","A"],["T","A"],["G","C"],["A","T"],["G","C"]].
+pairElement("CTCTA") //should return [["C","G"],["T","A"],["C","G"],["T","A"],["A","T"]]. 
