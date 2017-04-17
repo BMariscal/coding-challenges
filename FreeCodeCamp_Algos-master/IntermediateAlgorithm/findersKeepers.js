700251fc30d4e5b1eac79be0f61164cf062a012e
Briceida Mariscal <BMariscal@users.noreply.github.com>
2017-04-17 02:58:18 -0700

    findersKeepers

---
+++ FreeCodeCamp_Algos-master/IntermediateAlgorithm/findersKeepers.js
@@ -0,0 +1,17 @@
+
+//Create a function that looks through an array (first argument) and returns the first element
+//in the array that passes a truth test (second argument).
+
+
+
+function findElement(arr, func) {
+  
+  var keeper = arr.filter(func);
+  return keeper[0];
+}
+
+findElement([1, 2, 3, 4], function(num){ return num % 2 === 0; });
+
+
+findElement([1, 3, 5, 8, 9, 10], function(num) { return num % 2 === 0; }) //should return 8.
+findElement([1, 3, 5, 9], function(num) { return num % 2 === 0; }) //should return undefined.
