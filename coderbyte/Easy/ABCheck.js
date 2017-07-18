1f9a342b5a09f922f4ec962e6a8fa0f2953338d9
Bri Mariscal <BMariscal@users.noreply.github.com>
2017-07-17 22:20:22 -0700

    ABCheck.js

---
+++ coderbyte/Easy/ABCheck.js
@@ -0,0 +1,13 @@
+/*
+Have the function ABCheck(str) take the str parameter being passed and return the string true if the characters a and b are separated by exactly 3 places anywhere in the string at least once (ie. "lane borrowed" would result in true because there is exactly three characters between a and b). Otherwise return the string false. 
+
+Use the Parameter Testing feature in the box below to test your code with different arguments.*/
+
+function ABCheck(str) { 
+  if (str.toLowerCase().search(/a...b/gi) !== -1){
+      return true;
+  }else{
+      return false;
+  }
+}
+ABCheck(readline());
