f8ee0679e1031ef8ec66e6b1be9c69bce409c5a0
Briceida <BriceidaMariscal@gmail.com>
2019-11-20 10:52:17 -0800

    update problem 110

--- leetcode/110.py
+++ leetcode/110.py
@@ -1,15 +1,16 @@
 # Definition for a binary tree node.
-# class TreeNode:
-#     def __init__(self, x):
-#         self.val = x
-#         self.left = None
-#         self.right = None
+class TreeNode:
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
 
 class Solution:
     def __init__(self):
         self.isTreeBalanced = True
 
-    def isBalanced(self, root: TreeNode) -> bool:
+    def isBalanced(self, root):
         if not root:
             return self.isTreeBalanced
         self.maximumDepth(root)
@@ -21,6 +22,19 @@ class Solution:
         else:
             left_depth = self.maximumDepth(root.left)
             right_depth = self.maximumDepth(root.right)
+            print(left_depth, right_depth)
             if abs(left_depth - right_depth) > 1:
                 self.isTreeBalanced = False
             return max(left_depth, right_depth) + 1
+
+
+n = TreeNode(3)
+n1 = TreeNode(20)
+n.left = TreeNode(9)
+n.right = n1
+n1.left = TreeNode(15)
+n1.right = TreeNode(7)
+
+s = Solution()
+is_it = s.isBalanced(n)
+print(is_it)
