2fddd42e8f4b0dcd762e0feab32e3c721361fc6c
Briceida <BriceidaMariscal@gmail.com>
2019-12-12 14:26:17 -0800

    628

--- leetcode/628.py
+++ leetcode/628.py
@@ -2,7 +2,5 @@ class Solution:
     def maximumProduct(self, nums: List[int]) -> int:
         sort = sorted(nums)
         one = sort[0] * sort[1] * sort[-1]
-        two = sort[0] * sort[-2]  * sort[-1]
         three = sort[-3] * sort[-2] * sort[-1]
-        four = sort[0] * sort[1] * sort[2]
-        return max(one, two, three, four)
+        return max(one, three)
