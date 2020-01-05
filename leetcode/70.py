7f0948405f6876672b422db5ed875d02beccca02
Briceida <BriceidaMariscal@gmail.com>
2020-01-04 18:42:47 -0800

    dp 70

--- leetcode/70.py
+++ leetcode/70.py
@@ -4,6 +4,7 @@
 #
 # Note: Given n will be a positive integer.
 
+# O(n)
 
 class Solution:
     def climbStairs(self, n: int) -> int:
@@ -19,3 +20,16 @@ class Solution:
 
 
     # input: 35, output: 14930352
+
+
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        dp = [0] * (n + 1)
+        dp[0] = 1
+
+        dp[1] = 1
+
+        for i in range(2, n + 1):
+            dp[i] = dp[i - 1] + dp[i - 2]
+
+        return dp[n]
